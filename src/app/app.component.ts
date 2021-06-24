de19c12ec47593040583dd52dedbc9c1bd18c504
mfrancekovic <francekovic.marko@gmail.com>
2021-06-24 10:51:42 +0200

    Wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'css-fundamentals';
+
+  pFixed1 = false;
+  pSticky1 = false;
+  pRelative1 = false;
+  pAbsolute1 = false;
+
+  pExample1 = false;
+  pExample2 = false;
+  pExample3 = false;
+
 }
